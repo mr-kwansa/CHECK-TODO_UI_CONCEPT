3272bfc5a2967a76c73d2d9aad98df48f1baa2d2
mr-kwansa <a.kwansaamponsah@stu.ucc.edu.gh>
2023-03-17 22:52:14 +0000

    updates the site including js files to add functionality (under review) a signup or register and a  log in form

---
+++ index.js
@@ -0,0 +1,30 @@
+//function to show the login form when the button is cliked 
+function show_login_form(){
+    var show_login=document.getElementById("user_logins")
+    var button_1=document.getElementById("CREATE");
+    var button_2=document.getElementById("LOGIN_BTN");
+    //is statment to check is the form is being displayed or not if it is its hides it if not it displayes it 
+if(show_login.style.display="none"){
+    show_login.style.display="block";
+}
+//hide the old buttons when the forms display
+if(show_login.style.display="block"){
+    button_1.style.display="none";
+    button_2.style.display="none";
+}
+}
+//function to show the registration form when the button is cliked 
+function show_Reg_form(){
+    var show_reg=document.getElementById("user_details");
+    var button_1=document.getElementById("CREATE");
+    var button_2=document.getElementById("LOGIN_BTN");
+    //is statment to check is the form is being displayed or not if it is its hides it if not it displayes it 
+if(show_reg.style.display="none"){
+    show_reg.style.display="block";
+}
+//hide the old buttons when the forms display
+if(show_reg.style.display="block"){
+    button_1.style.display="none";
+    button_2.style.display="none";
+}
+}
